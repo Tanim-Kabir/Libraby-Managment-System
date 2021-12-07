5c8a2e90d3f38a19497cbf6a1a776a024756b5f2
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2021-12-08 01:00:42 +0600

    Create demo index and style file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		LMS
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<h1 style="color: white;">LIBRARY MANAGEMENT SYSTEM</h1>
+		</div>
+
+		</header>
+
+	</div>
+</body>
+</html>
