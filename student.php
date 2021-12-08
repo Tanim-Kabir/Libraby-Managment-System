79dd35d1379cfc1296c77c465214c8a2e10febaf
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2021-12-08 11:15:24 +0600

    add bootstrap for table

--- student.php
+++ student.php
@@ -12,13 +12,16 @@
 	<title>Student</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <h3>List Of Student</h3>
     <?php
     $result = mysqli_query($db,"SELECT * FROM `student`;");
     ?>
-    <table>
+    <table class="table table-bordered">
         <tr style='background-color: #1c33d76b;'>
             <th>First Name</th>
             <th>Last Name</th>
